121b0b4b38990e641f4b48154d521c02aa19f11a
Peter Alaxin <peter.alaxin@webaster.sk>
2012-12-16 07:11:24 +0100

    Validator aggregation class

---
+++ Validator.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Validator class
+ *
+ * Luki framework
+ * Date 16.12.2012
+ *
+ * @version 3.0.0
+ *
+ * @author Peter Alaxin, <alaxin@almex.sk>
+ * @copyright (c) 2009, Almex spol. s r.o.
+ * * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ * @package Luki
+ * @subpackage Class
+ * @filesource
+ */
+
+/**
+ * Validation aggregation class
+ *
+ * @package Luki
+ */
+class Luki_Validator {
+
+	private static $sError = '';
+
+	public static function isValid($xValue, $sValidator, $aOptions = array(), $sMessage = '')
+	{
+		$sValidator = 'Luki_Validator_' . $sValidator;
+		$oValidator = new $sValidator($aOptions);
+		self::$sError = '';
+
+		if(!empty($sMessage)) {
+			$oValidator->setMessage($sMessage);
+		}
+
+		$bReturn = $oValidator->isValid($xValue);
+
+		if(!$bReturn) {
+			self::$sError = $oValidator->getError();
+		}
+
+		unset($xValue, $sValidator, $aOptions, $oValidator, $sMessage);
+		return $bReturn;
+	}
+
+	public static function getError()
+	{
+		return self::$sError;
+	}
+
+}
+
+# End of file
