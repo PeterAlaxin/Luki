90a6693d7a8aca3383851fc9313301e8efb8479e
Peter Alaxin <peter.alaxin@webaster.sk>
2013-10-06 10:47:15 +0200

    Link filter

---
+++ Template/Filters/Link.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Link template filter adapter
+ *
+ * Luki framework
+ * Date 22.3.2013
+ *
+ * @version 3.0.0
+ *
+ * @author Peter Alaxin, <alaxin@almex.sk>
+ * @copyright (c) 2009, Almex spol. s r.o.
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ * @package Luki
+ * @subpackage Class
+ * @filesource
+ */
+
+namespace Luki\Template\Filters;
+
+use Luki\Url;
+
+/**
+ * Link template filter
+ * 
+ * @package Luki
+ */
+class Link {
+
+	public function Get($sValue)
+	{
+		$sReturn = Url::makeLink($sValue);
+		
+		unset($sValue);
+		return $sReturn;
+	}
+}
+
+# End of file
