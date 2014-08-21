da1c5050d664fc84812212e524d9005849d5b0ef
peter <alaxin@almex.sk>
2014-08-21 19:36:50 +0200

    If block text fix

--- Template/Block.php
+++ Template/Block.php
@@ -180,7 +180,7 @@ class Block
         foreach ( $matches as $match ) {
 
             $subMatches = array();
-            preg_match_all('/\(|\)|is blank|is not blank|is constant|is not constant|is defined|is not defined|is even|is not even|is iterable|is not iterable|is null|is not null|is odd|is not odd|is sameas|is not sameas|==|===|!=|\<|\>|\<=|\>=|[a-z0-9_\."\']*|\+|-/', $match[1], $subMatches, PREG_SET_ORDER);
+            preg_match_all('/\(|\)|is blank|is not blank|is constant|is not constant|is defined|is not defined|is even|is not even|is iterable|is not iterable|is null|is not null|is odd|is not odd|is sameas|is not sameas|==|===|!=|\<|\>|\<=|\>=|".*"|[a-z0-9_\."\']*|\+|-/', $match[1], $subMatches, PREG_SET_ORDER);
             $condition = '';
 
             foreach ( $subMatches as $subMatch ) {
