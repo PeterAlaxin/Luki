1c7687194969f359c07b0d8bfd6ce57bc6b91e11
peter <alaxin@almex.sk>
2014-09-03 08:42:49 +0200

    Fix split UTF string

--- Template/Filters/Split.php
+++ Template/Filters/Split.php
@@ -27,46 +27,60 @@ namespace Luki\Template\Filters;
 class Split
 {
 
-    public function Get($value, $separator = '', $limit = 0)
+    public function Get($value, $separator = '', $limit = 1)
     {
         switch ( gettype($value) ) {
             case 'string':
                 if ( empty($separator) ) {
-                    $values = preg_split("//u", $value, NULL, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
-
-                    if ( $limit > 0 ) {
-                        $newValue = array();
-                        $count = 0;
-                        $item = '';
-
-                        foreach ( $values as $value ) {
-                            $item .= $value;
-                            $count++;
-
-                            if ( $count == $limit ) {
-                                $count = 0;
-                                $newValue[] = $item;
-                                $item = '';
-                            }
-                        }
-
-                        if ( !empty($item) ) {
-                            $newValue[] = $item;
-                        }
-
-                        $values = $newValue;
-                    }
+                    $split = $this->_splitWithoutSeparator($value, $limit);
                 } else {
-                    $values = preg_split("/" . $separator . "/u", $value, $limit, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+                    $split = $this->_splitWithSeparator($value, $separator, $limit);
                 }
-
-                $split = $values;
                 break;
             default:
                 $split = $value;
         }
 
-        unset($value, $values);
+        unset($value, $separator, $limit);
+        return $split;
+    }
+
+    private function _splitWithoutSeparator($value, $limit)
+    {
+        if ( $limit > 0 ) {
+            $split = array();
+            $len = mb_strlen($value, "UTF-8");
+            for ( $i = 0; $i < $len; $i += $limit ) {
+                $split[] = mb_substr($value, $i, $limit, "UTF-8");
+            }
+        } else {
+            $split = preg_split("//u", $value, -1, PREG_SPLIT_NO_EMPTY);
+        }
+
+        unset($value, $limit, $len, $i);
+        return $split;
+    }
+
+    private function _splitWithSeparator($value, $separator, $limit)
+    {
+        $values = explode($separator, $value);
+
+        if ( $limit > 1 ) {
+            $split = array();
+            $last = array();
+            foreach ( $values as $key => $value ) {
+                if ( $key + 1 < $limit ) {
+                    $split[] = $value;
+                } else {
+                    $last[] = $value;
+                }
+            }
+            $split[] = implode($separator, $last);
+        } else {
+            $split = $values;
+        }
+
+        unset($value, $separator, $limit, $values, $key, $last);
         return $split;
     }
 
