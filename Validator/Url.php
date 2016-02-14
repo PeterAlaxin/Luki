e94e20d1bf1d1a1ddf1a260cea93a0b8ff1ecce5
peter <alaxin@almex.sk>
2016-02-14 21:58:14 +0100

    Fix URL validation

--- Validator/Url.php
+++ Validator/Url.php
@@ -29,6 +29,8 @@ use Luki\Validator\basicFactory;
 class Url extends basicFactory
 {
 
+    private $regex = '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS';
+    
     public function __construct($options = array())
     {
         parent::__construct($options);
@@ -41,9 +43,8 @@ class Url extends basicFactory
     public function isValid($value)
     {
         $this->isValid = FALSE;
-
-        if ( FALSE !== filter_var($value, FILTER_VALIDATE_URL) or 
-             FALSE !== filter_var('http://' . $value, FILTER_VALIDATE_URL) ) {
+        
+        if ( empty($value) or 1 === preg_match($this->regex, trim($value, '!"#$%&\'()*+,-./@:;<=>[\\]^_`{|}~')) ) {
             $this->setNoError();
         } else {
             $this->fillMessage('/%value%/', $value);
