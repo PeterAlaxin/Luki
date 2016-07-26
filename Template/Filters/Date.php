3f64e30f82502c0fa1dea6fd06e234f3e8ff0cf4
peter <alaxin@almex.sk>
2016-07-26 11:14:15 +0200

    Add human format date

--- Template/Filters/Date.php
+++ Template/Filters/Date.php
@@ -42,10 +42,11 @@ class Date
             $date = new \DateTime($value, $timeZone);
         }
 
-        if(strpos($format, '%') === FALSE) {
+        if ( 'human' == $format ) {
+            $formatedDate = $this->getHuman($date->getTimestamp());
+        } elseif ( strpos($format, '%') === FALSE ) {
             $formatedDate = $date->format($format);
-        }
-        else {
+        } else {
             $formatedDate = strftime($format, $date->getTimestamp());
         }
 
@@ -53,6 +54,65 @@ class Date
         return $formatedDate;
     }
 
-}
+    private function getHuman($timestamp)
+    {       
+        if ($timestamp >= strtotime('+7 day 00:00')) {            
+            $text = strftime('%e. %B %G', $timestamp);
+fd($text, 'a');            
+        } elseif ($timestamp >= strtotime('+2 day 00:00')) {
+            $text = $this->translate('Next ') . strftime('%A, %k:%M', $timestamp);
+fd($text, 'b');            
+        } elseif ($timestamp >= strtotime('tomorrow 00:00')) {
+            $text = $this->translate('Tomorrow at ') . strftime('%k:%M', $timestamp);
+fd($text, 'c');            
+        } elseif ($timestamp >= strtotime('today 00:00')) {
+            $text = strftime('%k:%M', $timestamp);
+fd($text, 'd');            
+        } elseif ($timestamp >= strtotime('yesterday 00:00')) {
+            $text = $this->translate('Yesterday at ') . strftime('%k:%M', $timestamp);
+fd($text, 'e');            
+        } elseif ($timestamp >= strtotime('-6 day 00:00')) {
+            $text = strftime('%A, %k:%M', $timestamp);
+fd($text, 'f');            
+        } else {
+            $text = strftime('%e. %B %G', $timestamp);
+fd($text, 'g');            
+        }
 
-# End of file
+        return $text;
+    }
+    
+    private function translate($text)
+    {
+        if('sk_SK' == locale_get_default()) {
+            $tran = $this->translateToSlovak($text); 
+        }
+        else {
+            $tran = $text;
+        }
+        
+        unset($text);
+        return $tran;
+    }
+    
+    private function translateToSlovak($text)
+    {
+        switch($text) {
+            case 'Tomorrow at ':
+                $tran = 'Zajtra o ';
+                break;
+            case 'Yesterday at ':
+                $tran = 'Včera o ';
+                break;
+            case 'Next ':
+                $tran = 'Budúci ';
+                break;
+            default :
+                $tran = $text;
+        }
+        
+        unset($text);
+        return $tran;
+    }
+
+}
