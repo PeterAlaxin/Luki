5eff7eabcae4b41cf1e4c794b98ddb9b8b16ba4d
peter <alaxin@almex.sk>
2016-10-03 19:05:20 +0200

    Add UrlExist validator

---
+++ Validator/UrlExist.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * UrlExist validator
+ *
+ * Luki framework
+ *
+ * @author Peter Alaxin, <peter@lavien.sk>
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ * @package Luki
+ * @subpackage Validator
+ * @filesource
+ */
+
+namespace Luki\Validator;
+
+use Luki\Validator\BasicFactory;
+
+class UrlExist extends BasicFactory
+{
+
+	public function __construct($options = array())
+	{
+		parent::__construct($options);
+		$this->setMessage('The URL \'%value%\' does not exists!');
+	}
+
+	public function isValid($value)
+	{
+		$this->isValid = false;
+
+		$headers = @get_headers($value);
+		if (strpos($headers[0], '200') !== false) {
+			$this->setNoError();
+		} else {
+			$this->fillMessage('/%value%/', $value);
+		}
+
+		return $this->isValid;
+	}
+}
