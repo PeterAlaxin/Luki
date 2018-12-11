b9470241e3f8806b15025635b82833f79cc5c3c6
Peter Alaxin <peter@lavien.sk>
2018-12-11 14:54:09 +0100

    Add MD5 filter

---
+++ Template/Filters/Md5.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Md5 template filter adapter
+ *
+ * Luki framework
+ *
+ * @author Peter Alaxin, <peter@lavien.sk>
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ * @package Luki
+ * @subpackage Template
+ * @filesource
+ */
+
+namespace Luki\Template\Filters;
+
+class Md5
+{
+
+    public function Get($value)
+    {
+        $md5 = md5($value);
+
+        return $md5;
+    }
+}
