f9c2cb90a2b6247ff6381e282a454791604abfdd
Peter Alaxin <peter@lavien.sk>
2019-02-27 11:30:27 +0100

    Update Functions

--- Functions.php
+++ Functions.php
@@ -4,10 +4,26 @@ use Luki\Storage;
 
 function default_exception_handler($e)
 {
-    echo '<h2>Something is wrong!</h2>';
-    echo '<p>'.$e->getMessage().'</p>';
-    echo '<pre>';
-    echo '</pre>';
+    echo '<h2>Something is wrong!</h2>'."\n";
+
+    if (Storage::isProfiler()) {
+        Storage::Profiler()->setError($e);
+    }
+
+    if (Storage::isDevelopment()) {
+        echo 'Error  : '.$e->getMessage()."<br>\n";
+        echo 'In file: '.$e->getFile()."<br>\n";
+        echo 'On line: '.$e->getLine()."<br>\n";
+
+        foreach ($e->getTrace() as $item) {
+            if (empty($item['file'])) {
+                echo 'Function: '.$item['function']."<br>\n";
+            } else {
+                echo 'Called from: '.$item['file'].' Line: '.$item['line'].' Function: '.$item['function']."<br>\n";
+            }
+        }
+        exit;
+    }
 }
 
 function fd($value, $name = null)
@@ -29,8 +45,26 @@ function camelCase($str, array $noStrip = array())
 
 function _t($text, $section = '')
 {
+    $lng         = Storage::Get('langPage');
+    $translation = Storage::Language()->Get($text, $lng, $section);
+
+    return $translation;
+}
+
+function _ta($text, $section = '', $fill = null)
+{
     $lng         = Storage::Get('lng');
     $translation = Storage::Language()->Get($text, $lng, $section);
+
+    if (is_array($fill)) {
+        foreach ($fill as $from => $to) {
+            $translation = str_replace($from, $to, $translation);
+        }
+    }
+
+    if ($text == $translation) {
+        $translation .= '['.$section.']';
+    }
 
     return $translation;
 }
