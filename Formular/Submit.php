9b50b1d6a72ad130657366ed9832a6d4b2091cac
Peter Alaxin <peter@lavien.sk>
2019-10-16 10:23:54 +0200

    Add new input type submit

---
+++ Formular/Submit.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Submit input
+ *
+ * Luki framework
+ *
+ * @author Peter Alaxin, <peter@lavien.sk>
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ * @package Luki
+ * @subpackage Formular
+ * @filesource
+ */
+
+namespace Luki\Formular;
+
+use Luki\Formular\BasicFactory;
+
+class Submit extends BasicFactory
+{
+
+    public function __construct($name, $label, $placeholder = '')
+    {
+        #parent::__construct($name, $label, $placeholder);
+        $this->setType('submit');
+    }
+}
